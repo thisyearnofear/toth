164ba7e1bd68f193882da934e704ded16b3fca2c
Quincybob <74205675+Quincybob@users.noreply.github.com>
2025-12-28 02:30:59 +0300

    chore: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "TOTH",
   description: "Community tipping rewards in $degen on farcaster",
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎩</text></svg>',
+  },
 };
 
 export default function RootLayout({
